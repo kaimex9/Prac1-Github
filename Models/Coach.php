b0c66ef940e9a0322ab8d5c3a77cd17ecf9cf302
Jonathancano2003 <124157297+Jonathancano2003@users.noreply.github.com>
2024-09-23 16:57:40 +0200

    Update Coach.php

--- Models/Coach.php
+++ Models/Coach.php
@@ -1,39 +1,116 @@
+<?php
+
 namespace App\Models;
 
+
 /**
- * Represents any person.
+ * Represents coach.
  */
-class Person
+class Coach extends Person
 {
     /**
-     * Name of the person
+     * Coach name
      *
      * @var string
      */
     private $name;
 
     /**
-     * Status of the person (active/inactive)
+     * Coach Status
      *
+
      * @var boolean
      */
     private $active;
 
+
     /**
-     * Tactic of the person (if coach)
+     * Coach tactic
      *
      * @var string
      */
     private $tactic;
 
-    /**
-     * Age of the person
-     *
-     * @var int
-     */
-    private $age;
 
     /**
-     * Construct a Person with a name, status, age, and possibly a tactic.
+     * Construct a Coach with a name and the status.
      *
-     * @param string $name   Name of the person
+     * @param string $name     Coach name
+     * @param bool $active  Coach Status
+     * @param string $tactic La nueva táctica a implementar
+     */
+
+    public function __construct(string $name, bool $active, string $tactic)
+    {
+        $this->name = $name;
+        $this->active = $active;
+        $this->tactic = $tactic;
+    }
+    /**
+     * Get Coach name
+     *
+     * @return  string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * Get Coach Status
+     *
+     * @return  bool
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    public function getTactic()
+    {
+        return $this->tactic;
+    }
+
+    /**
+     * Set Coach name in string
+     *
+     * @param  string  $name is string
+     *
+     * @return  self
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+    /**
+     * Set status in bool
+     *
+     * @param  bool  $active is true or false
+     *
+     * @return  self
+     */
+    public function setActive(bool $active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Set Coach name in string
+     *
+     * @param  string  $tactic is string
+     *
+     * @return  self
+     */
+    public function deviseTeamTactic(string $tactic): void
+    {
+        echo "{$this->name} says: Alright team, our new tactic for the next game will be '{$tactic}'!";
+    }
+    
+    function train()
+    {
+        echo "{$this->name} says: Come on Team lets go train!!!!!";
+    }
+}
